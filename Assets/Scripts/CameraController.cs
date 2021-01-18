ab814c631b1eaa0a4e5e469c75ea3ed10bea805f
Sebastian Penaherrera <sebasspenaherrera@gmail.com>
2021-01-18 21:57:38 +0100

    Third version

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // ATTRIBUTES
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = Vector3.zero;
+        position.z = -10;
+        position.x = player.position.x;
+        position.y = player.position.y;
+
+        this.transform.position = position;
+    }
+}
